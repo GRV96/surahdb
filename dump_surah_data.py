e1d66d69e12d287c6f53bf4390e7148475ba889d
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2025-04-24 21:19:24 -0400

    Module `file_io.py`

--- dump_surah_data.py
+++ dump_surah_data.py
@@ -1,16 +1,17 @@
-import csv
-import json
 from pathlib import Path
 from sys import argv
 
 import mysql.connector
 
+from file_io import\
+	load_json_file,\
+	write_csv
+
 
 auth_path = Path(argv[1]).resolve()
 data_path = Path(argv[2]).resolve()
 
-with auth_path.open("r", encoding="utf-8") as auth_file:
-	authentication = json.load(auth_file)
+authentication = load_json_file(auth_path)
 
 db_conn = mysql.connector.connect(**authentication)
 with db_conn.cursor() as cursor:
@@ -18,9 +19,5 @@ with db_conn.cursor() as cursor:
 	cursor.execute("SELECT * FROM v_chron_order;")
 	surah_data = cursor.fetchall()
 
-	with data_path.open("w", encoding="utf-8", newline="") as data_file:
-		writer = csv.writer(data_file, delimiter=";")
-		writer.writerow(("id", "chronology", "titlefr", "period", "nbverses"))
-
-		for sd in surah_data:
-			writer.writerow(sd)
+	column_titles = ("id", "chronology", "titlefr", "period", "nbverses")
+	write_csv(data_path, column_titles, surah_data)
--- load_surah_data.py
+++ load_surah_data.py
@@ -1,8 +1,11 @@
-import json
 from pathlib import Path
 from sys import argv
 
 import mysql.connector
+
+from file_io import\
+	load_json_file,\
+	read_whole_file
 
 
 BACKSLASH = "\\"
@@ -12,15 +15,13 @@ SLASH = "/"
 auth_path = Path(argv[1]).resolve()
 data_path = Path(argv[2]).resolve()
 
-with auth_path.open("r", encoding="utf-8") as auth_file:
-	authentication = json.load(auth_file)
+authentication = load_json_file(auth_path)
 
 db_conn = mysql.connector.connect(**authentication)
 
-init_db_path = Path("init_db.sql").resolve()
 with db_conn.cursor() as cursor:
-	with init_db_path.open("r", encoding="utf-8") as init_db_file:
-		cursor.execute(init_db_file.read())
+	script_content = read_whole_file(Path("init_db.sql").resolve())
+	cursor.execute(script_content)
 
 db_conn = mysql.connector.connect(**authentication)
 with db_conn.cursor() as cursor:
