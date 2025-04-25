392de084170288808d8e4181cdf3565c62302bb3
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2025-04-24 20:29:20 -0400

    Loading successful

---
+++ init_db.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE IF NOT EXISTS surahdb;
+USE surahdb;
+SET GLOBAL local_infile = true;
+
+CREATE TABLE IF NOT EXISTS surahs(
+    id INT PRIMARY KEY UNIQUE, -- The surah's number in the Quran
+    chronology INT,
+    titlefr VARCHAR(40),
+    period INT,
+    nbverses INT
+);
+
+CREATE OR REPLACE VIEW v_chron_order AS
+SELECT *
+FROM surahs
+ORDER BY chronology;
+
+CREATE OR REPLACE VIEW v_trad_order AS
+SELECT *
+FROM surahs
+ORDER BY id;
---
+++ load_surah_data.py
@@ -0,0 +1,35 @@
+import json
+from pathlib import Path
+from sys import argv
+
+import mysql.connector
+
+
+BACKSLASH = "\\"
+SLASH = "/"
+
+
+auth_path = Path(argv[1]).resolve()
+data_path = Path(argv[2]).resolve()
+
+with auth_path.open("r", encoding="utf-8") as auth_file:
+	authentication = json.load(auth_file)
+
+db_conn = mysql.connector.connect(**authentication)
+
+init_db_path = Path("init_db.sql").resolve()
+with db_conn.cursor() as cursor:
+	with init_db_path.open("r", encoding="utf-8") as init_db_file:
+		cursor.execute(init_db_file.read())
+
+db_conn = mysql.connector.connect(**authentication)
+with db_conn.cursor() as cursor:
+	cursor.execute("USE surahdb;")
+	cursor.execute("SET GLOBAL local_infile = true;")
+	cursor.execute(
+		f"LOAD DATA LOCAL INFILE \'{str(data_path).replace(BACKSLASH, SLASH)}\'\n"
+		"INTO TABLE surahs\n"
+		"FIELDS TERMINATED BY ';'\n"
+		"IGNORE 1 LINES\n"
+		"(id, chronology, titlefr, period, nbverses);")
+	db_conn.commit()
