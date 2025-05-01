05f5af8bd8d4f1a19328b291602d89e0e308bd07
Guyllaume Rousseau <32419088+GRV96@users.noreply.github.com>
2025-05-01 19:56:06 -0400

    Module `db_reading.py` (#3)
    It provides tuple `COLUMN_TITLES` and function `get_surah_data`.

--- dump_surah_data.py
+++ dump_surah_data.py
@@ -2,6 +2,9 @@ import mysql.connector
 
 from arg_parser import\
 	make_dumping_parser
+from db_reading import\
+	COLUMN_TITLES,\
+	get_surah_data
 from file_io import\
 	load_json_file,\
 	write_csv
@@ -15,11 +18,5 @@ chron_order = args.chron_order
 authentication = load_json_file(auth_path)
 
 db_conn = mysql.connector.connect(**authentication)
-with db_conn.cursor() as cursor:
-	view = "v_chron_order" if chron_order else "v_trad_order"
-	cursor.execute("USE surahdb;")
-	cursor.execute(f"SELECT * FROM {view};")
-	surah_data = cursor.fetchall()
-
-	column_titles = ("id", "chronology", "titlefr", "period", "nbverses")
-	write_csv(data_path, column_titles, surah_data)
+surah_data = get_surah_data(db_conn, chron_order)
+write_csv(data_path, COLUMN_TITLES, surah_data)
