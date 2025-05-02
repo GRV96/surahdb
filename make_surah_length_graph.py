cd65f10262132ed3e09c01e6e105a439350c6671
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2025-05-01 21:21:31 -0400

    A graph shows the surahs' length.

--- make_surah_length_graph.py
+++ make_surah_length_graph.py
@@ -18,5 +18,17 @@ authentication = load_json_file(auth_path)
 db_conn = mysql.connector.connect(**authentication)
 surah_length_data = get_surahs_length(db_conn, chron_order)
 
-for sl in surah_length_data[:6]:
-	print(f"{sl[0]}: {sl[1]}")
+surah_ids = list()
+surah_lengths = list()
+for sl in surah_length_data:
+	surah_ids.append(sl[0])
+	surah_lengths.append(sl[1])
+
+plt.bar(surah_ids, surah_lengths)
+
+plt.title("Length of the Surahs")
+plt.xlabel("Surah number")
+plt.xlim(0, 114)
+plt.ylabel("Number of verses")
+plt.tight_layout()
+plt.show()
