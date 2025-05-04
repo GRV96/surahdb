2d950f2781300362242f90b9e0c51fe8b04d19dd
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2025-05-04 17:25:27 -0400

    Package `src` and its modules declare `__all__`.

--- src/__init__.py
+++ src/__init__.py
@@ -2,3 +2,11 @@ from . import arg_parser
 from . import db_reading
 from . import file_io
 from . import surah_graphs
+
+
+__all__ = [
+	arg_parser.__name__,
+	db_reading.__name__,
+	file_io.__name__,
+	surah_graphs.__name__
+]
