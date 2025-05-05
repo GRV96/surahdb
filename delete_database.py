257568e892e277dd9b542f1e389d959242734939
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2025-05-04 21:55:58 -0400

    [7] Script `delete_database.py` works.

---
+++ delete_database.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+from src.arg_parser import\
+	make_auth_parser
+from src.file_io import\
+	load_json_file
+
+
+args = make_auth_parser().parse_args()
+auth_path = args.auth_path.resolve()
+
+authentication = load_json_file(auth_path)
+
+db_conn = mysql.connector.connect(**authentication)
+
+with db_conn.cursor() as cursor:
+	cursor.execute("DROP DATABASE IF EXISTS surahdb;")
