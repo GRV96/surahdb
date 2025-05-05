36dfd7f736e46b307ad44fb3fa4eaea423790b15
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2025-05-04 23:04:40 -0400

    [7] Function `db_reading.db_exists`

--- delete_database.py
+++ delete_database.py
@@ -2,6 +2,9 @@ import mysql.connector
 
 from src.arg_parser import\
 	make_parser_auth
+from src.db_reading import\
+	DB_NAME_SURAHDB,\
+	db_exists
 from src.file_io import\
 	load_json_file
 
@@ -13,10 +16,7 @@ authentication = load_json_file(auth_path)
 db_conn = mysql.connector.connect(**authentication)
 
 with db_conn.cursor() as cursor:
-	cursor.execute("SHOW DATABASES LIKE 'surahdb';")
-	db_matches = cursor.fetchall()
-
-	if len(db_matches) == 0:
-		print("Database surahdb does not exist.")
+	if db_exists(cursor, DB_NAME_SURAHDB):
+		cursor.execute(f"DROP DATABASE IF EXISTS {DB_NAME_SURAHDB};")
 	else:
-		cursor.execute("DROP DATABASE IF EXISTS surahdb;")
+		print("Database surahdb does not exist.")
