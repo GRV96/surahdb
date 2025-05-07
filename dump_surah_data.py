d7e10581e3c5389bb16bddcae3cd84e318c17898
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2025-05-07 17:40:47 -0400

    [10] `get_surah_data` is the only function that reads table `surahs`.

--- dump_surah_data.py
+++ dump_surah_data.py
@@ -3,7 +3,8 @@ import mysql.connector
 from src.arg_parser import\
 	make_parser_dumping
 from src.db_reading import\
-	COLUMN_TITLES,\
+	COLUMN_NAMES,\
+	PERIOD_UNDEF,\
 	get_surah_data
 from src.file_io import\
 	load_json_file,\
@@ -18,5 +19,5 @@ chron_order = args.chron_order
 authentication = load_json_file(auth_path)
 db_conn = mysql.connector.connect(**authentication)
 
-surah_data = get_surah_data(db_conn, chron_order)
-write_csv(data_path, COLUMN_TITLES, surah_data)
+surah_data = get_surah_data(db_conn, chron_order, PERIOD_UNDEF)
+write_csv(data_path, COLUMN_NAMES, surah_data)
