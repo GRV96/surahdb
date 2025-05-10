fd6897e9fbd602319ebbe073b39f219a5589e564
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2025-05-09 21:43:48 -0400

    [8] Legend displayed, two plots in the same `Axes` object.

--- plot_progression_in_reading.py
+++ plot_progression_in_reading.py
@@ -12,9 +12,14 @@ from src.db_reading import\
 from src.file_io import\
 	load_json_file
 from src.surah_graphs import\
-	GRAPH_X_LIMIT,\
+	COLOR_MECCAN,\
+	COLOR_MEDINAN,\
+	LABEL_MECCAN,\
+	LABEL_MEDINAN,\
+	X_LIMIT,\
+	X_TICKS,\
 	apply_order,\
-	make_axes_values_cumulength
+	make_axes_values
 
 
 args = make_parser_plots().parse_args()
@@ -26,20 +31,32 @@ db_conn = mysql.connector.connect(**authentication)
 
 surah_per_len_data = get_surah_data(db_conn, chron_order, PERIOD_UNDEF,
 	COLUMN_ID, COLUMN_PERIOD, COLUMN_NB_VERSES)
-surah_numbers, nb_verses_read, colors\
-	= make_axes_values_cumulength(surah_per_len_data)
+surah_nums_mec, surah_nums_med, surah_lengths_mec, surah_lengths_med\
+	= make_axes_values(surah_per_len_data, True)
 
-graph_title_suffix, x_indices = apply_order(chron_order, surah_numbers)
+graph_title_suffix, x_indices_mec, x_indices_med\
+	= apply_order(chron_order, surah_nums_mec, surah_nums_med)
+
+if chron_order:
+	x_labels = get_surah_data(db_conn, True, PERIOD_UNDEF, COLUMN_ID)
+	x_labels = *(item[0] for item in x_labels),
+else:
+	x_labels = X_TICKS
 
 figure = plt.figure()
 ax = figure.add_subplot(1, 1, 1)
-ax.scatter(x_indices, nb_verses_read, color=colors)
+line = ax.scatter(x_indices_mec, surah_lengths_mec,
+	color=COLOR_MECCAN, label=LABEL_MECCAN)
+print(type(line))
+ax.scatter(x_indices_med, surah_lengths_med,
+	color=COLOR_MEDINAN, label=LABEL_MEDINAN)
 ax.set_title("Progression in the Quran's Reading" + graph_title_suffix)
 ax.set_xlabel("Last surah read")
-ax.set_xlim(0, GRAPH_X_LIMIT)
-ax.set_xticks(x_indices, labels=surah_numbers, rotation=90, fontsize=9)
+ax.set_xlim(0, X_LIMIT)
+ax.set_xticks(X_TICKS, labels=x_labels, rotation=90, fontsize=9)
 ax.set_ylabel("Verses read")
 ax.set_yticks(range(0, 6001, 500), minor=True)
 ax.grid(True, which="both")
 figure.tight_layout()
+plt.legend()
 plt.show()
--- plot_surah_length.py
+++ plot_surah_length.py
@@ -12,7 +12,12 @@ from src.db_reading import\
 from src.file_io import\
 	load_json_file
 from src.surah_graphs import\
-	GRAPH_X_LIMIT,\
+	COLOR_MECCAN,\
+	COLOR_MEDINAN,\
+	LABEL_MECCAN,\
+	LABEL_MEDINAN,\
+	X_LIMIT,\
+	X_TICKS,\
 	apply_order,\
 	make_axes_values
 
@@ -26,19 +31,30 @@ db_conn = mysql.connector.connect(**authentication)
 
 surah_per_len_data = get_surah_data(db_conn, chron_order, PERIOD_UNDEF,
 	COLUMN_ID, COLUMN_PERIOD, COLUMN_NB_VERSES)
-surah_numbers, surah_lengths, colors\
-	= make_axes_values(surah_per_len_data)
+surah_nums_mec, surah_nums_med, surah_lengths_mec, surah_lengths_med\
+	= make_axes_values(surah_per_len_data, False)
 
-graph_title_suffix, x_indices = apply_order(chron_order, surah_numbers)
+graph_title_suffix, x_indices_mec, x_indices_med\
+	= apply_order(chron_order, surah_nums_mec, surah_nums_med)
+
+if chron_order:
+	x_labels = get_surah_data(db_conn, True, PERIOD_UNDEF, COLUMN_ID)
+	x_labels = *(item[0] for item in x_labels),
+else:
+	x_labels = X_TICKS
 
 figure = plt.figure()
 ax = figure.add_subplot(1, 1, 1)
-ax.bar(x_indices, surah_lengths, color=colors)
+ax.bar(x_indices_mec, surah_lengths_mec,
+	color=COLOR_MECCAN, label=LABEL_MECCAN)
+ax.bar(x_indices_med, surah_lengths_med,
+	color=COLOR_MEDINAN, label=LABEL_MEDINAN)
 ax.set_title("Length of the Surahs" + graph_title_suffix)
 ax.set_xlabel("Surah number")
-ax.set_xlim(0, GRAPH_X_LIMIT)
-ax.set_xticks(x_indices, labels=surah_numbers, rotation=90, fontsize=9)
+ax.set_xlim(0, X_LIMIT)
+ax.set_xticks(X_TICKS, labels=x_labels, rotation=90, fontsize=9)
 ax.set_ylabel("Length (verses)")
 ax.grid(True, axis="y")
 figure.tight_layout()
+plt.legend()
 plt.show()
