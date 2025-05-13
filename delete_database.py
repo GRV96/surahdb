149d19f5862a4aeef537faadb27e4742473211db
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2025-05-12 22:11:33 -0400

    [15] All scripts validate the database configuration.

--- delete_database.py
+++ delete_database.py
@@ -2,6 +2,8 @@ import mysql.connector
 
 from src.arg_parser import\
 	make_parser_db_config
+from src.database.db_config_validation import\
+	validate_db_config
 from src.database.db_reading import\
 	DB_NAME_SURAHDB,\
 	db_exists
@@ -13,6 +15,7 @@ args = make_parser_db_config().parse_args()
 db_config_path = args.db_config.resolve()
 
 db_config = load_json_file(db_config_path)
+validate_db_config(db_config)
 db_conn = mysql.connector.connect(**db_config)
 
 with db_conn.cursor() as cursor:
