9fc66d0ea32cc7eaba48102d50730d6457f5ed06
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2025-05-14 22:11:04 -0400

    [16] Function `validate_db_config` has a docstring.

--- src/database/db_config_validation.py
+++ src/database/db_config_validation.py
@@ -8,7 +8,18 @@ from src.file_io import load_json_file
 _CONFIG_SCHEMA_PATH = Path(__file__).resolve().parent/"db_config_schema.json"
 
 
-def validate_db_config(db_config):
+def validate_db_config(db_config: dict) -> None:
+	"""
+	Given a configuration for the connection to a MySQL server, this function
+	validates it according to the schema.
+
+	Args:
+		db_config: the connection's configuration
+
+	Raises:
+		jsonschema.exceptions.ValidationError: if the connection configuration
+			does not match the schema.
+	"""
 	_db_config_schema = load_json_file(_CONFIG_SCHEMA_PATH)
 	validate(db_config, _db_config_schema)
 
