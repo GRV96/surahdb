a49ed621d4ed638737f56757641da32eb89df761
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2025-05-16 10:53:00 -0400

    [16] The docstring of `validate_db_config` tells when the configuration is valid.

--- src/database/db_config_validation.py
+++ src/database/db_config_validation.py
@@ -11,7 +11,8 @@ _CONFIG_SCHEMA_PATH = Path(__file__).resolve().parent/"db_config_schema.json"
 def validate_db_config(db_config: dict) -> None:
 	"""
 	Given a configuration for the connection to a MySQL server, this function
-	validates it according to the schema.
+	validates it according to the schema. If no exception is raised, the
+	configuration is valid.
 
 	Args:
 		db_config: the connection's configuration
