0ff3f088a605d5e6a90446b39cac7e39a06c33b9
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2025-06-20 17:01:21 -0400

    [31] `Language`.`_missing_` is overridden.

--- src/surah_graphs/language.py
+++ src/surah_graphs/language.py
@@ -5,5 +5,18 @@ class Language(StrEnum):
 	ENGLISH = "en"
 	FRENCH = "fr"
 
+	@classmethod
+	def _missing_(cls, value):
+		if not isinstance(value, str):
+			raise TypeError(f"{cls.__name__} values are strings.")
+
+		value_lower = value.lower()
+
+		for language in Language:
+			if language == value_lower:
+				return language
+
+		raise ValueError(f"{value} is not a {cls.__name__} value.")
+
 
 __all__ = [Language.__name__]
